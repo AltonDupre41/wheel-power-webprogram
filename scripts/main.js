309db0bca5ca6b902068d65b90acd8468af1c25e
Alton Dupre <altondupre@eatel.net>
2024-10-14 14:24:29 -0500

    Added the durability system with a progress bar along with it, soon there needs to be a repair button added

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 const clickButton = document.getElementById('click-btn');
 const progressBar = document.getElementById('progress-bar');
+const durabilityBar = document.getElementById('durability-bar');
 const clickCountDisplay = document.getElementById('click-count');
 const autoClickCountDisplay = document.getElementById('auto-click-count');
+
+let autoClickActive = false
+let autoClickBroken = false
+let autoClickDurability = 10
+let autoClickMaxDurability = 10
 
 let clickCount = 0;
 let autoClickCount = 0;
@@ -35,8 +41,22 @@ function updateProgress() {
     }
 }
 
+function updateDurability(){
+    //move progress bar
+    let durability = (autoClickDurability / autoClickMaxDurability) * 100;
+    if (durability > 100) durability = 100;
+    durabilityBar.style.width = (durability) + '%';
+
+    //change progress bar colors
+    const red = Math.min(durability * 2.55, 255);
+    const green = Math.max(255 - (durability * 2.55), 0);
+    durabilityBar.style.backgroundColor = `rgb(${green}, ${red}, 0)`;
+
+}
+
 //manual click function
 clickButton.addEventListener('click', () => {
+    if (!autoClickActive){autoClickActive = true;}
     clickCount++;
     clickCountDisplay.textContent = clickCount;
     updateProgress();
@@ -44,6 +64,28 @@ clickButton.addEventListener('click', () => {
 
 //autoclick function
 setInterval(() => {
+    if (!autoClickActive || autoClickBroken){return;} 
+    autoClickCount++;
+    autoClickCountDisplay.textContent = autoClickCount;
+    updateProgress();
+    
+    if (autoClickDurability <= 0) {
+        autoClickBroken = true
+        updateDurability()
+        //TODO: add a repair button to repair the auto clicker, how its repaired could just be a simple, Press 4 buttons
+    }
+    else {
+        autoClickDurability--;
+        updateDurability()
+    }
+    
+
+}, 1000); //auto click every X000 (every 1000 is 1 sec)
+
+
+//autoclick function for when the autoclicker is broken
+setInterval(() => {
+    if (!autoClickActive || !autoClickBroken){return;} 
     autoClickCount++;
     autoClickCountDisplay.textContent = autoClickCount;
     updateProgress();
